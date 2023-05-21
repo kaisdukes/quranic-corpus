68119381343c18b514c372e3ceb4d93403ad118c
Kais Dukes <dukeskais79@gmail.com>
2023-05-21 20:53:52 +0100

    Improve word-by-word layout.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,7 +6,8 @@ export const Home = () => {
         <div className='home'>
             <h1>Corpus 2.0: Word by Word Test</h1>
             <p>
-                This is a test page optimized for viewing on a desktop.
+                This design is currently at a basic stage, serving as a color test for word segments.
+                Rest assured, this is not the final appearance, which will undergo significant enhancements.
             </p>
             <WordByWordView />
         </div>
