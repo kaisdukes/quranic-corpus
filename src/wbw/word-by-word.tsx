d0ce8f03789d81211af3417480adbc4ff0e71f29
Kais Dukes <dukeskais79@gmail.com>
2023-05-22 14:50:51 +0100

    Make word-by-word a top-level page component.

--- src/wbw/word-by-word.tsx
+++ src/wbw/word-by-word.tsx
@@ -1,8 +1,12 @@
+import { Fragment, useEffect, useRef, useState } from 'react';
 import { NavigationContainer } from '../navigation/navigation-container';
 import { NavigationHeader } from '../navigation/navigation-header';
-import { WordByWordView } from './word-by-word-view';
 import { ChapterService } from '../corpus/orthography/chapter-service';
 import { formatChapterTitle } from '../corpus/orthography/chapter';
+import { MorphologyService } from '../corpus/morphology/morphology-service';
+import { VerseElement } from '../treebank/verse-element';
+import { Verse } from '../corpus/orthography/verse';
+import { Footer } from '../components/footer';
 import { container } from 'tsyringe';
 import './word-by-word.scss';
 
@@ -14,12 +18,69 @@ export const WordByWord = ({ chapterNumber }: Props) => {
     const chapterService = container.resolve(ChapterService);
     const chapter = chapterService.getChapter(chapterNumber);
 
+    const [verses, setVerses] = useState<Verse[]>([]);
+    const loadingRef = useRef<HTMLDivElement>(null);
+    const morphologyService = container.resolve(MorphologyService);
+    const [loading, setLoading] = useState(false);
+    const [chapterEnd, setChapterEnd] = useState(false);
+
+    const loadVerses = async (startVerseNumber: number) => {
+        setLoading(true);
+        console.log('LOADING VERSE ' + startVerseNumber);
+        const newVerses = await morphologyService.getMorphology([chapterNumber, startVerseNumber], 5);
+        if (newVerses.length > 0) {
+            setVerses(prevVerses => [...prevVerses, ...newVerses]);
+        } else {
+            setChapterEnd(true);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        setVerses([]);
+        setChapterEnd(false);
+        loadVerses(1);
+    }, [chapterNumber]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+            if (entry.isIntersecting && !loading && !chapterEnd) {
+                loadVerses(verses.length + 1);
+            }
+        }, {
+            rootMargin: '0px',
+            threshold: 0.1
+        });
+
+        if (loadingRef.current) {
+            observer.observe(loadingRef.current);
+        }
+
+        return () => {
+            if (loadingRef.current) {
+                observer.unobserve(loadingRef.current);
+            }
+        };
+    }, [verses, loading, chapterEnd]);
+
     return (
         <NavigationContainer header={<NavigationHeader chapterNumber={chapterNumber} />}>
-            <h1 className='chapter-title'>{chapterNumber}. {formatChapterTitle(chapter)}</h1>
-            {
-                <WordByWordView chapterNumber={chapterNumber} />
-            }
+            <div className='word-by-word'>
+                <h1 className='chapter-title'>{chapterNumber}. {formatChapterTitle(chapter)}</h1>
+                <div className='word-by-word-view'>
+                    {
+                        verses.map((verse, i) => (
+                            <Fragment key={`verse-${i}`}>
+                                <VerseElement verse={verse} />
+                            </Fragment>
+                        ))
+                    }
+                </div>
+                <div>
+                    {loading && 'Loading...'}
+                </div>
+                <Footer ref={loadingRef} />
+            </div>
         </NavigationContainer>
     )
 }
