77f04d61eb6a2a47bd90687294ca00b419f30967
Kais Dukes <dukeskais79@gmail.com>
2023-05-22 06:46:46 +0100

    Improve navigation.

--- src/navigation/chapter-header.tsx
+++ src/navigation/chapter-header.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import './chapter-header.scss';
@@ -9,6 +10,21 @@ type Props = {
 export const ChapterHeader = ({ chapterNumber }: Props) => {
     const [showPopup, setShowPopup] = useState(false);
     const surahs = Array.from({ length: 114 }, (_, i) => i + 1);
+    const popupRef = useRef<HTMLUListElement | null>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+                setShowPopup(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     const togglePopup = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
         e.preventDefault();
@@ -18,7 +34,7 @@ export const ChapterHeader = ({ chapterNumber }: Props) => {
     return (
         <div className={`chapter-header ${showPopup ? 'show-popup' : ''}`}>
             <a href='#' onClick={togglePopup}>Surah {chapterNumber}</a>
-            <ul className='chapter-popup'>
+            <ul className='chapter-popup' ref={popupRef}>
                 {surahs.map(surah => (
                     <li key={surah}>
                         <Link to={`/${surah}`} onClick={() => setShowPopup(false)}>Surah {surah}</Link>
