0f45078e49c4eccb7b231df73f8ab4f59111396e
Elijah <aa1719@imperial.ac.uk>
2023-05-22 19:06:18 +0100

    Frontend: Refactored verse loading to be bi-directional. Fix auto load previous.

--- src/wbw/word-by-word.tsx
+++ src/wbw/word-by-word.tsx
@@ -1,4 +1,5 @@
 import { Fragment, useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { NavigationContainer } from '../navigation/navigation-container';
 import { NavigationHeader } from '../navigation/navigation-header';
 import { ChapterService } from '../corpus/orthography/chapter-service';
@@ -11,57 +12,121 @@ import { container } from 'tsyringe';
 import makkah from '../images/makkah.svg';
 import madinah from '../images/madinah.svg';
 import './word-by-word.scss';
-import {useParams} from "react-router-dom";
 
-
-export const WordByWord = ({}) => {
+export const WordByWord = () => {
     const { chapterNumber, verseNumber } = useParams();
     const parsedChapterNumber = Number(chapterNumber);
     const parsedVerseNumber = Number(verseNumber);
+
+    // TODO:// update the verses type to include these params
+    const [startVerse, setStartVerse] = useState(parsedVerseNumber);
+    const [endVerse, setEndVerse] = useState(parsedVerseNumber + 5);
+
+    // Used to make sure the previous verses don't automatically cause a rerender
+    const [loadingPrevious, setLoadingPrevious] = useState(false);
+
+
     const chapterService = container.resolve(ChapterService);
     const chapter = chapterService.getChapter(parsedChapterNumber);
 
     const [verses, setVerses] = useState<Verse[]>([]);
-    const loadingRef = useRef<HTMLDivElement>(null);
+
+    const topLoadingRef = useRef<HTMLDivElement>(null);
+    const bottomLoadingRef = useRef<HTMLDivElement>(null);
+
     const morphologyService = container.resolve(MorphologyService);
     const [loading, setLoading] = useState(false);
     const [chapterEnd, setChapterEnd] = useState(false);
 
-    const loadVerses = async (startVerseNumber: number) => {
+    const loadNextVerses = async () => {
         setLoading(true);
-        console.log('LOADING VERSE ' + startVerseNumber);
-        const newVerses = await morphologyService.getMorphology([parsedChapterNumber, startVerseNumber], 5);
+        console.log('LOADING NEXT VERSES ' + endVerse);
+        const newVerses = await morphologyService.getMorphology([parsedChapterNumber, endVerse], 5);
+        setLoading(false);
         if (newVerses.length > 0) {
             setVerses(prevVerses => [...prevVerses, ...newVerses]);
+            setEndVerse(endVerse + 5);
         } else {
             setChapterEnd(true);
         }
-        setLoading(false);
     };
+
+    const loadPreviousVerses = async () => {
+        setLoadingPrevious(true);
+        setLoading(true);
+        console.log('LOADING PREVIOUS VERSE ' + startVerse);
+        try {
+            const newVerses = await morphologyService.getMorphology([parsedChapterNumber, Math.max(0, startVerse - 5)], 5);
+            setLoading(false);
+            if (newVerses.length > 0) {
+                setVerses(prevVerses => [...newVerses, ...prevVerses]);
+                setStartVerse(Math.max(0, startVerse - 5));
+            }
+            // Add a delay before resetting loadingPrevious
+            setTimeout(() => setLoadingPrevious(false), 200);
+        } catch (err) {
+            console.log(err)
+            setLoadingPrevious(false);
+        }
+
+    };
+
+
+    // const loadPreviousVerses = async () => {
+    //     setLoading(true);
+    //     console.log('LOADING PREVIOUS VERSE ' + startVerse);
+    //     try {
+    //         const newVerses = await morphologyService.getMorphology([parsedChapterNumber, Math.max(0, startVerse - 5)], 5);
+    //         setLoading(false);
+    //         if (newVerses.length > 0) {
+    //             setVerses(prevVerses => [...newVerses, ...prevVerses]);
+    //             setStartVerse(Math.max(0, startVerse - 5));
+    //         }
+    //     } catch (error) {
+    //         console.error(error);  // This will log more information about the error
+    //     }
+    // };
+
 
     useEffect(() => {
         setVerses([]);
         setChapterEnd(false);
-        loadVerses(1);
-    }, [chapterNumber]);
+        loadNextVerses().catch((err) => {console.log(err)});
+    }, [parsedChapterNumber, parsedVerseNumber]);
 
     useEffect(() => {
-        const observer = new IntersectionObserver(([entry]) => {
-            if (entry.isIntersecting && !loading && !chapterEnd) {
-                loadVerses(verses.length + 1);
+        const observerTop = new IntersectionObserver(([entry]) => {
+            if (entry.isIntersecting && !loading && !loadingPrevious) {
+                loadPreviousVerses();
             }
         }, {
             rootMargin: '0px',
             threshold: 0.1
         });
 
-        if (loadingRef.current) {
-            observer.observe(loadingRef.current);
+        const observerBottom = new IntersectionObserver(([entry]) => {
+            if (entry.isIntersecting && !loading && !chapterEnd) {
+                loadNextVerses();
+            }
+        }, {
+            rootMargin: '0px',
+            threshold: 0.1
+        });
+
+        if (topLoadingRef.current) {
+            observerTop.observe(topLoadingRef.current);
+        }
+
+        if (bottomLoadingRef.current) {
+            observerBottom.observe(bottomLoadingRef.current);
         }
 
         return () => {
-            if (loadingRef.current) {
-                observer.unobserve(loadingRef.current);
+            if (topLoadingRef.current) {
+                observerTop.unobserve(topLoadingRef.current);
+            }
+            if (bottomLoadingRef.current) {
+                observerBottom.unobserve(bottomLoadingRef.current);
             }
         };
     }, [verses, loading, chapterEnd]);
@@ -71,9 +136,10 @@ export const WordByWord = ({}) => {
             <div className='word-by-word'>
                 <div className='chapter-header'>
                     <img src={chapter.city === 'Makkah' ? makkah : madinah} />
-                    <h1>{chapterNumber}. {formatChapterTitle(chapter)}</h1>
+                    <h1>{parsedChapterNumber}. {formatChapterTitle(chapter)}</h1>
                 </div>
                 <div className='word-by-word-view'>
+                    <div ref={topLoadingRef}>Loading...</div>
                     {
                         verses.map((verse, i) => (
                             <Fragment key={`verse-${i}`}>
@@ -81,11 +147,12 @@ export const WordByWord = ({}) => {
                             </Fragment>
                         ))
                     }
+                    <div ref={bottomLoadingRef}>Loading...</div>
                 </div>
                 <div>
                     {loading && 'Loading...'}
                 </div>
-                <Footer ref={loadingRef} />
+                <Footer />
             </div>
         </NavigationContainer>
     )
