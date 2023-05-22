087acfaa53e0456dd7c3fdc623f2920c8f30c80c
Kais Dukes <dukeskais79@gmail.com>
2023-05-22 05:28:39 +0100

    Add Slack invitation.

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../images/logo.svg';
+import slack from '../images/slack.svg';
+
 import './home.scss';
 
 export const Home = () => {
@@ -16,11 +18,16 @@ export const Home = () => {
             <p>
                 <Link className='wbw' to='/1'>Quran Word by Word</Link>
             </p>
-            <hr/>
+            <hr />
             <div className='footer'>
                 <div>Copyright &copy; Kais Dukes, 2009-2023.</div>
                 <div><a href='https://eps.leeds.ac.uk/computing-applied-computing/staff/33/professor-eric-atwell'>Artificial Intelligence for Language</a></div>
                 <div><a href='https://www.leeds.ac.uk'>University of Leeds</a></div>
+                <div className='slack'>
+                    <a href='https://join.slack.com/t/quraniccorpus/shared_invite/zt-1vrmewq5c-TfFf~I7W5e6v3VYDeJsyvw'>
+                        <img src={slack} />
+                    </a>
+                </div>
             </div>
         </div>
     )
