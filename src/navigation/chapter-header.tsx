c8ba84e1b59e4a4cd82ddb5a95d75a88f77f402f
Kais Dukes <dukeskais79@gmail.com>
2023-05-22 06:32:43 +0100

    Chapter popup (WiP).

--- src/navigation/chapter-header.tsx
+++ src/navigation/chapter-header.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './chapter-header.scss';
 
 type Props = {
@@ -5,9 +7,24 @@ type Props = {
 }
 
 export const ChapterHeader = ({ chapterNumber }: Props) => {
+    const [showPopup, setShowPopup] = useState(false);
+    const surahs = Array.from({ length: 114 }, (_, i) => i + 1);
+
+    const togglePopup = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        e.preventDefault();
+        setShowPopup(!showPopup);
+    }
+
     return (
-        <div className='chapter-header'>
-            <a href='#'>Surah {chapterNumber}</a>
+        <div className={`chapter-header ${showPopup ? 'show-popup' : ''}`}>
+            <a href='#' onClick={togglePopup}>Surah {chapterNumber}</a>
+            <ul className='chapter-popup'>
+                {surahs.map(surah => (
+                    <li key={surah}>
+                        <Link to={`/${surah}`}>Surah {surah}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
