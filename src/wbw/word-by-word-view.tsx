8580793ad931368ab99d9076352594ac9f2b9690
Kais Dukes <dukeskais79@gmail.com>
2023-05-22 01:40:08 +0100

    Infinite scroll (WiP).

--- src/wbw/word-by-word-view.tsx
+++ src/wbw/word-by-word-view.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import { MorphologyService } from '../corpus/morphology/morphology-service';
 import { VerseElement } from '../treebank/verse-element';
 import { Verse } from '../corpus/orthography/verse';
@@ -7,26 +7,58 @@ import './word-by-word-view.scss';
 
 export const WordByWordView = () => {
 
-    const [verses, setVerses] = useState<Verse[]>();
+    const [verses, setVerses] = useState<Verse[]>([]);
+    const loadingRef = useRef<HTMLDivElement>(null);
     const morphologyService = container.resolve(MorphologyService);
-    const location = [2, 255];
+    const location = [78, 1];
+    const [startChapterNumber, startVerseNumber] = location;
+    const [loading, setLoading] = useState(false);
+
+    const loadVerses = async () => {
+        setLoading(true);
+        const newVerses = await morphologyService.getMorphology([startChapterNumber, startVerseNumber + verses.length], 5);
+        setVerses(prevVerses => [...prevVerses, ...newVerses]);
+        setLoading(false);
+    };
 
     useEffect(() => {
-        (async () => {
-            setVerses(await morphologyService.getMorphology(location, 5));
-        })();
+        loadVerses();
     }, []);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+            if (entry.isIntersecting && !loading) {
+                loadVerses();
+            }
+        }, {
+            rootMargin: '0px',
+            threshold: 0.1
+        });
+
+        if (loadingRef.current) {
+            observer.observe(loadingRef.current);
+        }
+
+        return () => {
+            if (loadingRef.current) {
+                observer.unobserve(loadingRef.current);
+            }
+        };
+    }, [verses, loading]);
 
     return (
         <div className='word-by-word-view'>
             {
-                verses && verses.map((verse, i) => (
+                verses.map((verse, i) => (
                     <Fragment key={`verse-${i}`}>
                         {i > 0 && <hr />}
                         <VerseElement verse={verse} />
                     </Fragment>
                 ))
             }
+            <div ref={loadingRef}>
+                {loading && 'Loading...'}
+            </div>
         </div>
-    )
-}
+    );
+};
