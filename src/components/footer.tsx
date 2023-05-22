5be2ae078a7a9ab2394f789aeb88326e935203d0
Kais Dukes <dukeskais79@gmail.com>
2023-05-23 00:22:59 +0100

    Language research group.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = forwardRef<HTMLDivElement, {}>((_, ref) => {
     return (
         <div ref={ref} className='footer'>
             <div>Copyright &copy; <a href='https://www.linkedin.com/in/kaisdukes'>Dr. Kais Dukes</a>, 2009-2023</div>
-            <div><a href='https://eps.leeds.ac.uk/computing-applied-computing/staff/33/professor-eric-atwell'>Artificial Intelligence for Language</a></div>
+            <div><a href='https://eps.leeds.ac.uk/computing-applied-computing/staff/33/professor-eric-atwell'>Language Research Group</a></div>
             <div><a href='https://eps.leeds.ac.uk/computing'>University of Leeds</a></div>
         </div>
     )
