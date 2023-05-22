d0e11a1cc26bad85060a6309cd29242e59707f28
Kais Dukes <dukeskais79@gmail.com>
2023-05-22 23:35:40 +0100

    Update intro.

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -4,20 +4,30 @@ import { Footer } from '../components/footer';
 import slack from '../images/slack.svg';
 import './home.scss';
 
-export const Home = () => {
-    const randomChapter = Math.floor(Math.random() * 114) + 1;
+const randomChapter = () => Math.floor(Math.random() * 114) + 1;
 
+export const Home = () => {
     return (
         <div className='home'>
+            <Link to={`/${randomChapter()}`}>
+                <Logo className='logo' />
+            </Link>
             <h1>The Quranic Arabic Corpus</h1>
-            <Logo className='logo' />
             <p className='intro'>
-                The Quranic Arabic Corpus is a valuable resource that provides detailed linguistic
-                insights for every word in the Quran. It includes three types of analysis: morphology
-                (the structure of words), syntax (the grammar of sentences), and semantics (meaning).
+                The world's most popular site for learning Quranic Arabic.
+                An AI analyzed the entire quran, and the resulting data was reivewed by experts, to provide insights into the Quran's structure (syntax), word
+                formation (morpology), and meaning (semantics).
+            </p>
+            <p style={{marginTop: 0}}>
+                To help leaners, the corpus features unique color-coded grammar diagrams that are
+                based on <em>i’rāb</em> (إعراب), the traditional Arabic science of linguistics.
             </p>
             <p className='wbw'>
-                <Link to={`/${randomChapter}`}>Quran Word by Word</Link>
+                <Link to={`/${randomChapter()}`}>Quran Word by Word</Link>
+            </p>
+            <p>
+                The corpus provides a supportive community for learning Quranic Arabic. It is a free, open-source,
+                Wikipedia-style project, and we encourage collobration, discusson and continuous improvement.
             </p>
             <div className='slack'>
                 <a href='https://join.slack.com/t/quraniccorpus/shared_invite/zt-1vrmewq5c-TfFf~I7W5e6v3VYDeJsyvw'>
