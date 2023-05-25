f915924d148c2cbd11e00e26278f5c47141e6e0e
Kais Dukes <dukeskais79@gmail.com>
2023-05-25 11:40:28 +0100

    Toggle detail mode icon.

--- src/navigation/toolbar.tsx
+++ src/navigation/toolbar.tsx
@@ -1,21 +1,23 @@
 import { IconButton } from '../components/icon-button';
 import { useReaderSettings } from '../context/reader-settings-context';
 import read from './../images/icons/read.svg'
+import detail from './../images/icons/detail.svg'
 import './toolbar.scss';
 
 export const Toolbar = () => {
     const { readerSettings, setReaderSettings } = useReaderSettings();
+    const { readerMode } = readerSettings;
 
     const toggleReaderMode = () => {
         setReaderSettings({
             ...readerSettings,
-            readerMode: !readerSettings.readerMode
+            readerMode: !readerMode
         });
     };
 
     return (
         <div className='toolbar'>
-            <IconButton icon={read} onClick={toggleReaderMode} />
+            <IconButton icon={readerMode ? detail : read} onClick={toggleReaderMode} />
         </div>
     )
 }
