76fcd1319f3b3c683b1c0c5283a175c881bcb8ce
Kais Dukes <dukeskais79@gmail.com>
2023-05-25 10:01:52 +0100

    Fix chapter header alignment content on mobile.

--- src/wbw/chapter-header.tsx
+++ src/wbw/chapter-header.tsx
@@ -14,7 +14,7 @@ export const ChapterHeader = ({ chapter }: Props) => {
         <div className='chapter-header'>
             <img src={chapter.city === 'Makkah' ? makkah : madinah} />
             <div>
-                Sūrat {phonetic}
+                <span className='phonetic'>Sūrat {phonetic}</span>
                 {
                     translation &&
                     <>
