2584b8987fef757e0b2639323855139e17e11f53
Kais Dukes <dukeskais79@gmail.com>
2023-05-29 08:39:58 +0100

    Add Qaf logo to the left of navigation header.

--- src/navigation/navigation-header.tsx
+++ src/navigation/navigation-header.tsx
@@ -1,9 +1,11 @@
 import { useRef, useEffect } from 'react';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { ChapterService } from '../corpus/orthography/chapter-service';
 import { ChevronDown } from '../components/chevron-down';
 import { Toolbar } from './toolbar';
 import { VerseSelector } from './verse-selector';
+import { Qaf } from './qaf';
 import { container } from 'tsyringe';
 import './navigation-header.scss';
 
@@ -13,13 +15,19 @@ type Props = {
 
 export const NavigationHeader = ({ chapterNumber }: Props) => {
     const [showPopup, setShowPopup] = useState(false);
+    const chapterNameRef = useRef<HTMLAnchorElement | null>(null);
     const popupRef = useRef<HTMLDivElement | null>(null);
     const chapterService = container.resolve(ChapterService);
     const chapter = chapterService.getChapter(chapterNumber);
 
     useEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
-            if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+            const targetElement = event.target as Node;
+            if (chapterNameRef.current && chapterNameRef.current.contains(targetElement)) {
+                return;
+            }
+
+            if (popupRef.current && !popupRef.current.contains(targetElement)) {
                 setShowPopup(false);
             }
         };
@@ -32,6 +40,7 @@ export const NavigationHeader = ({ chapterNumber }: Props) => {
     }, []);
 
     const togglePopup = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        console.log('Toggle popup...');
         e.preventDefault();
         setShowPopup(!showPopup);
     }
@@ -39,7 +48,10 @@ export const NavigationHeader = ({ chapterNumber }: Props) => {
     return (
         <div className='navigation-header'>
             <div className='navigation-bar'>
-                <a href='#' onClick={togglePopup}>
+                <Link to='/'>
+                    <Qaf />
+                </Link>
+                <a ref={chapterNameRef} className='chapter-name' href='#' onClick={togglePopup}>
                     {chapterNumber}. {chapter.phonetic}
                     <ChevronDown className='down' />
                 </a>
---
+++ src/navigation/qaf.tsx
@@ -0,0 +1,10 @@
+import './qaf.scss';
+import qaf from '../images/qaf-white.svg';
+
+export const Qaf = () => {
+    return (
+        <div className='qaf'>
+            <img src={qaf} />
+        </div>
+    )
+}
