ce83482b9d37920de87d666a49f497c8a42e49d1
Kais Dukes <dukeskais79@gmail.com>
2023-05-30 02:03:29 +0100

    Simplify treebank comparison test.

--- src/treebank/treebank.tsx
+++ src/treebank/treebank.tsx
@@ -6,44 +6,14 @@ export const Treebank = () => {
         <div className='treebank'>
             <h1>Corpus 2.0: Renderer Test</h1>
             <p>
-                This is a test page optimized for viewing on a desktop. Its purpose is to evaluate a new vector
-                graphics renderer for the upcoming 2.0 version of the Quranic Arabic Corpus. We're comparing two
-                images here: the current bitmap-rendered image on the left from the existing corpus, and the new
-                vector-rendered image on the right. Please note, the image on the right is still under development
-                and doesn't completely replicate the dependency graph yet.
+                This desktop-optimized page tests a new vector renderer for Quranic Arabic Corpus 2.0.
+                We're comparing the under-development vector-rendered image (first) with the existing
+                bitmap-rendered image (second). The vector image may not fully replicate the dependency
+                graph yet.
             </p>
             <div className='compare'>
-                <div>
-                    <h3>Current: Bitmap Graphics Rendering</h3>
-                    <div className='bitmap-container'>
-                        <table className='token-table'>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <span className='location'>(4:79:15)</span><br />
-                                        <a href='https://corpus.quran.com//qurandictionary.jsp?q=rsl#(4:79:15)'>rasūlan</a><br />
-                                        (as) a Messenger
-                                    </td>
-                                    <td>
-                                        <span className='location'>(4:79:14)</span><br />
-                                        <a href='https://corpus.quran.com/qurandictionary.jsp?q=nws#(4:79:14)'>lilnnāsi</a><br />
-                                        for the people
-                                    </td>
-                                    <td>
-                                        <span className='location'>(4:79:13)</span><br />
-                                        <a href='https://corpus.quran.com//qurandictionary.jsp?q=rsl#(4:79:13)'>wa-arsalnāka</a><br />
-                                        And We have sent you
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <img src='https://corpus.quran.com/graphimage?id=2553' />
-                    </div>
-                </div>
-                <div>
-                    <h3>Prototype: Vector Graphics Rendering</h3>
-                    <DependencyGraphView />
-                </div>
+                <DependencyGraphView />
+                <img src='https://corpus.quran.com/graphimage?id=2553' />
             </div>
         </div>
     )
