477738566f4a2af1d6664fc29ec1632f79b17d68
Kais Dukes <dukeskais79@gmail.com>
2023-06-02 07:09:36 +0100

    Add license to footer.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,12 @@ export const Footer = () => {
     return (
         <footer>
             <div>Copyright &copy; <a href='https://www.linkedin.com/in/kaisdukes'>Dr. Kais Dukes</a>, 2009-2023.</div>
-            <div><a href='https://eps.leeds.ac.uk/computing-applied-computing/staff/33/professor-eric-atwell'>Language Research Group,</a></div>
-            <div><a href='https://eps.leeds.ac.uk/computing'>University of Leeds.</a></div>
+            <div>
+                <a href='https://eps.leeds.ac.uk/computing-applied-computing/staff/33/professor-eric-atwell'>Language Research Group</a>, <a href='https://eps.leeds.ac.uk/computing'>University of Leeds.</a>
+            </div>
+            <div>
+                The Quranic Arabic Corpus is <a href='https://github.com/kaisdukes/quranic-corpus/blob/main/LICENSE'>open source</a>.
+            </div>
         </footer>
     )
 }
