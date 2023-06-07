8b0c224b0d330b4c483047638cf91f1f49d52d50
Kais Dukes <dukeskais79@gmail.com>
2023-06-07 09:51:51 +0100

    Fit height.

--- src/prototype/workspace.tsx
+++ src/prototype/workspace.tsx
@@ -9,11 +9,37 @@ export const Workspace = () => {
     return (
         <div className='workspace'>
             <SidePanel splitterPosition={leftSplitterPosition} onSplitterPositionChanged={setLeftSplitterPosition}>
-                Left Content
+                {
+                    (() => {
+                        const elements = [];
+                        for (let i = 1; i <= 20; i++) {
+                            elements.push(<div>Left {i}</div>)
+                        }
+                        return elements;
+                    })()
+                }
             </SidePanel>
-            <div className='main-content'>Main Content</div>
+            <div className='content'>
+                {
+                    (() => {
+                        const elements = [];
+                        for (let i = 1; i <= 30; i++) {
+                            elements.push(<div>Main {i}</div>)
+                        }
+                        return elements;
+                    })()
+                }
+            </div>
             <SidePanel right splitterPosition={rightSplitterPosition} onSplitterPositionChanged={setRightSplitterPosition}>
-                Right Content
+                {
+                    (() => {
+                        const elements = [];
+                        for (let i = 1; i <= 100; i++) {
+                            elements.push(<div>Right {i}</div>)
+                        }
+                        return elements;
+                    })()
+                }
             </SidePanel>
         </div>
     )
