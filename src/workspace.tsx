ed0be3a0e234b2b224ace6cbfa0509c94ccd5256
Kais Dukes <dukeskais79@gmail.com>
2023-06-11 20:30:10 +0100

    Toggle buttons.

--- src/workspace.tsx
+++ src/workspace.tsx
@@ -34,24 +34,37 @@ const Footer = ({ mobile }: FooterProps) => {
 export const Workspace = () => {
     const [mainContent, setMainContent] = useState(['Main']);
     const [infoContent, setInfoContent] = useState(['Info']);
+    const [focusMode, setFocusMode] = useState(false);
+    const [showInfo, setShowInfo] = useState(true);
 
     const addContent = () => {
         setMainContent(mainContent => [...mainContent, ...Array(10).fill('Main')]);
         setInfoContent(infoContent => [...infoContent, ...Array(5).fill('Info')]);
     }
 
+    const toggleFocusMode = () => setFocusMode(focusMode => !focusMode);
+    const toggleInfo = () => setShowInfo(showInfo => !showInfo);
+
     return (
         <>
             <div className='app-header'>HEADER</div>
             <div className='workspace'>
                 <div className='main'>
+                    <button onClick={toggleFocusMode}>{focusMode ? 'Focus Mode' : 'Normal Mode'}</button><br />
+                    {
+                        focusMode &&
+                        <><button onClick={toggleInfo}>{showInfo ? 'Info On' : 'Info Off'}</button><br /></>
+                    }
                     <TestView content={mainContent} />
                     <Footer />
                 </div>
-                <div className='info-panel'>
-                    <button onClick={addContent}>Add</button><br />
-                    <TestView content={infoContent} />
-                </div>
+                {
+                    showInfo &&
+                    <div className='info-panel'>
+                        <button onClick={addContent}>Add</button><br />
+                        <TestView content={infoContent} />
+                    </div>
+                }
                 <Footer mobile={true} />
             </div>
         </>
