2bd566df2e7e6361003b68aa4996a4825aa5655b
Kais Dukes <dukeskais79@gmail.com>
2023-06-14 02:04:50 +0100

    Use &ndash;

--- src/word-morphology/word-morphology-view.tsx
+++ src/word-morphology/word-morphology-view.tsx
@@ -41,7 +41,7 @@ export const WordMorphologyView = ({ wordMorphology }: Props) => {
                                         <div key={`description-${i}`} className='segment'>
                                             <NodeCircle className={className} />
                                             <div className='description'>
-                                                <strong className={className}>{segment.posTag}</strong> - <MarkupView markup={segmentDescriptions[i++]} />
+                                                <strong className={className}>{segment.posTag}</strong> &ndash; <MarkupView markup={segmentDescriptions[i++]} />
                                             </div>
                                         </div>
                                     )
